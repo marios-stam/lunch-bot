f8f2adfd4852bf7b186c662b2cd903f51b10cce3
Marios Stamatopoulos <mariosstamatopoulos1@gmail.com>
2022-11-05 03:06:15 +0100

    Demo Tekniken scraper Working

--- scrapperTeknikens.py
+++ scrapperTeknikens.py
@@ -11,32 +11,100 @@ import utils
 # from google_trans_new import google_translator
 
 
-def getWeekText():
-    # start by defining the options
-    options = webdriver.ChromeOptions()
-    options.headless = True  # it's more scalable to work in headless mode
-    # normally, selenium waits for all resources to download
-    # we don't need it as the page also populated with the running javascript code.
-    options.page_load_strategy = 'none'
-    # this returns the path web driver downloaded
-    chrome_path = ChromeDriverManager().install()
-    chrome_service = Service(chrome_path)
-    # pass the defined options and service objects to initialize the web driver
-    driver = Chrome(options=options, service=chrome_service)
-    driver.implicitly_wait(5)
+class TeknikensScrapper:
 
-    url = "https://www.teknikenshus.se/restaurang"
+    def __init__(self) -> None:
+        self.url = "https://www.teknikenshus.se/restaurang"
 
-    driver.get(url)
-    time.sleep(10)
+    def scrape(self):
+        # self.getWeekText()
+        self.weekText = loadWeekText()
+        self.splitWeekText()
 
-    # content = driver.find_element(By.CSS_SELECTOR, "div[class*='ItemsGridWithPostAtcRecommendations'")
-    # breads = content.find_elements(By.TAG_NAME, "li")
+        # Select today's menu
+        currentDay = utils.getWeekdayIndex()
+        currentDay = 3  # for testing
+        dayMenu = self.selectDay(currentDay)
 
-    week = driver.find_elements(By.CLASS_NAME, "matochmat-wrap")
-    week = week[0].text
+        # Translate today's menu
+        dayMenu = self.translateDay(dayMenu)
 
-    return week
+        for dish in dayMenu:
+            print(dish)
+
+        return dayMenu
+
+    def getWeekText(self):
+        # start by defining the options
+        options = webdriver.ChromeOptions()
+        options.headless = True  # it's more scalable to work in headless mode
+        # normally, selenium waits for all resources to download
+        # we don't need it as the page also populated with the running javascript code.
+        options.page_load_strategy = 'none'
+        # this returns the path web driver downloaded
+        chrome_path = ChromeDriverManager().install()
+        chrome_service = Service(chrome_path)
+        # pass the defined options and service objects to initialize the web driver
+        driver = Chrome(options=options, service=chrome_service)
+        driver.implicitly_wait(5)
+
+        url = self.url
+
+        driver.get(url)
+        time.sleep(10)
+
+        # content = driver.find_element(By.CSS_SELECTOR, "div[class*='ItemsGridWithPostAtcRecommendations'")
+        # breads = content.find_elements(By.TAG_NAME, "li")
+
+        week = driver.find_elements(By.CLASS_NAME, "matochmat-wrap")
+        week = week[0].text
+
+        self.weekText = week
+
+    def splitWeekText(self):
+        weekText = self.weekText
+
+        # split text into days
+        weekMenu = []
+
+        days = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag"]
+        for i, day in enumerate(days):
+
+            if i == len(days) - 1:
+                dayMenu = weekText.split(day)[1]
+            else:
+                dayMenu = weekText.split(day)[1].split(days[i+1])[0]
+
+            # remove first line (day and date)
+            dayMenu = dayMenu.splitlines()[1:]
+
+            # remove empty lines
+            dayMenu = [line for line in dayMenu if line.strip() != ""]
+
+            weekMenu.append(dayMenu)
+
+        self.weekMenu = weekMenu
+
+    def selectDay(self, dayIndex):
+        # select day
+        day = self.weekMenu[dayIndex]
+        if dayIndex == 4:
+            # discard last 4 lines
+            day = day[:-4]
+
+        return day
+
+    def translateDay(self, day, translator=None):
+        # translate swedish to english
+        if translator is None:
+            translator = Translator(service_urls=['translate.googleapis.com'])
+        # translator = google_translator()
+
+        for i, dish in enumerate(day):
+            translatedDish = utils.translateSwedish(dish, translator)
+            day[i] = translatedDish
+
+        return day
 
 
 def loadWeekText():
@@ -47,84 +115,7 @@ def loadWeekText():
     return week
 
 
-def translateSwedish(text, translator):
-    # translate swedish to english
-    # translation = translator.translate(text, src='sv', dest='en')
-    translation = translator.translate(text, dest='en')
-
-    return translation.text
-
-
-def splitWeekText(weekText):
-    # split text into days
-    weekMenu = []
-    translator = Translator(service_urls=['translate.googleapis.com'])
-    # translator = google_translator()
-    translate_text = translator.translate('Hola mundo!', lang_src='es', lang_tgt='en')
-
-    days = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag"]
-    for i, day in enumerate(days):
-
-        if i == len(days) - 1:
-            dayMenu = weekText.split(day)[1]
-        else:
-            dayMenu = weekText.split(day)[1].split(days[i+1])[0]
-
-        # remove first line (day and date)
-        dayMenu = dayMenu.splitlines()[1:]
-
-        # remove empty lines
-        dayMenu = [line for line in dayMenu if line.strip() != ""]
-
-        weekMenu.append(dayMenu)
-
-    # for i, day in enumerate(weekMenu):
-        # print("========================", days[i], "========================",)
-        # for j, dish in enumerate(day):
-        # translatedDish = translateSwedish(dish, translator)
-        # print(translatedDish)
-        # weekMenu[i][j] = translatedDish
-
-    return weekMenu
-
-
-def translateDay(day):
-    # translate swedish to english
-    translator = Translator(service_urls=['translate.googleapis.com'])
-    # translator = google_translator()
-    translate_text = translator.translate('Hola mundo!', lang_src='es', lang_tgt='en')
-
-    for i, dish in enumerate(day):
-        translatedDish = translateSwedish(dish, translator)
-        day[i] = translatedDish
-
-    return day
-
-
-def selectDay(dayIndex, weekMenu):
-    # select day
-    day = weekMenu[dayIndex]
-    if dayIndex == 4:
-        # discard last 4 lines
-        day = day[:-4]
-
-    return day
-
-
 if __name__ == "__main__":
-    # Get the week menu
+    teknikens = TeknikensScrapper()
 
-    # weekText = getWeekText()
-    weekText = loadWeekText()
-    weekMenu = splitWeekText(weekText)
-
-    # Select today's menu
-    currentDay = utils.getWeekdayIndex()
-    currentDay = 3  # for testing
-    dayMenu = selectDay(currentDay, weekMenu)
-
-    # Translate today's menu
-    dayMenu = translateDay(dayMenu)
-
-    for dish in dayMenu:
-        print(dish)
+    teknikens.scrape()
--- utils.py
+++ utils.py
@@ -5,5 +5,12 @@ def getWeekdayIndex():
     return datetime.today().weekday()
 
 
+def translateSwedish(text, translator):
+    # translate swedish to english
+    translation = translator.translate(text, dest='en')
+
+    return translation.text
+
+
 if __name__ == "__main__":
     print(getWeekdayIndex())
