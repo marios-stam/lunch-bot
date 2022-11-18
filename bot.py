616917dfe342d1858cb39ecb570b6266ce5421ef
Marios Stamatopoulos <mariosstamatopoulos1@gmail.com>
2022-11-18 18:26:54 +0100

    Fixed  Date

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ updater = Updater(botToken, use_context=True)
 def start(update, context):
     print("Start called")
     context.job_queue.run_daily(msg_spam,
-                                datetime.time(hour=17, minute=20, tzinfo=pytz.timezone('Europe/Stockholm')),
+                                datetime.time(hour=18, minute=28, tzinfo=pytz.timezone('Europe/Stockholm')),
                                 days=(0, 1, 2, 3, 4, 5, 6))
 
     # context.job_queue.run_repeating(msg_spam, interval=60)
